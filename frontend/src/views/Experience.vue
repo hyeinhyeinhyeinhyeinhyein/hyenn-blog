44dd771753f1fbcde18f88f0a74c16ca0dc92674
hyeinhyeinhyeinhyeinhyein <ttmmad@naver.com>
2025-06-18 23:13:57 +0900

    feat: add masonry-style layout for posta list

--- frontend/src/views/Experience.vue
+++ frontend/src/views/Experience.vue
@@ -1,24 +1,38 @@
 <template>
   <div class="experience-list">
-    <h2>게시물 목록</h2>
-    <ul>
-      <li v-for="item in items" :key="item.id" class="experience-item">
-        <router-link :to="`/experience/${item.id}`" class="item-link">
-          <!-- 미리보기 이미지 -->
+
+    <!-- Masonry 레이아웃 적용 -->
+    <ul class="masonry">
+      <li
+          v-for="item in items"
+          :key="item.id"
+          class="masonry-item"
+      >
+
+        <router-link
+            :to="`/experience/${item.id}`"
+            class="item-link"
+        >
+          <div class="experience-preview">
+          <small class="preview-no" style="color: #F6F6F6"> No. {{ item.id }} </small>
+          <div class="preview-header">
+
+          <div class="preview-title" style="color: #505050">{{ item.title }}</div>
+          </div>
+          <div class="preview-body">
           <img
               v-if="item.preview"
               :src="item.preview"
               alt="미리보기"
               class="preview-img"
           />
-          <!-- 제목 -->
-          <span class="item-title">{{ item.title }}</span>
+          </div>
+          </div>
         </router-link>
-        <small class="item-date">
-          ({{ formatDate(item.created_at) }})
-        </small>
+
       </li>
     </ul>
+
     <div class="write-link">
       <router-link to="/experience/text-editor">글쓰기</router-link>
     </div>
@@ -36,8 +50,9 @@ export default {
     };
   },
   async mounted() {
+    // mounted 훅에서 미리 게시글 조회
     try {
-      const res = await axios.get('/save/experience'); // 필요에 따라 '/list/experience'로 수정
+      const res = await axios.get('/save/experience');
       this.items = res.data.items.map(item => ({
         ...item,
         preview: this.extractFirstImage(item.content)
@@ -48,17 +63,14 @@ export default {
   },
   methods: {
     extractFirstImage(content) {
-      // 1) Markdown 이미지 ![alt](url) 패턴
       const md = /!\[.*?\]\((.*?)\)/.exec(content);
       if (md && md[1]) return md[1];
-      // 2) HTML <img src="url"> 패턴
       const html = /<img[^>]+src=["']([^"']+)["']/.exec(content);
       if (html && html[1]) return html[1];
       return null;
     },
     formatDate(dateString) {
-      const d = new Date(dateString);
-      return d.toLocaleString();
+      return new Date(dateString).toLocaleString();
     }
   }
 };
@@ -66,58 +78,77 @@ export default {
 
 <style scoped>
 .experience-list {
-  max-width: 800px;
-  margin: 0 auto;
-  padding: 1rem;
+  margin-top: 2.5vw;
+  margin-left: 5vw;
+  margin-right: 5vw;
+
 }
 
-.experience-list h2 {
-  margin-bottom: 1rem;
+.experience-preview {
+
 }
 
-.experience-list ul {
-  list-style: none;
+.preview-header{
+  padding-top: 2vh;
+  padding-bottom: 2vh;
+  padding-left: 2vw;
+  padding-right: 2vw;
+  background-color: #F6F6F6;
+  border: 1px solid #D9D9D9;
+}
+.preview-body{
+  border: 1px solid #D9D9D9;
+}
+
+/* Masonry 컨테이너: 3열, 간격 1rem */
+.masonry {
+  column-count: 5;
+  column-gap: 1rem;
   padding: 0;
+  margin: 0;
+  list-style: none;
 }
 
-.experience-item {
-  display: flex;
-  align-items: center;
-  margin: 0.75em 0;
+/* 컬럼 안의 아이템: inline-block + break-inside로 분할 방지 */
+.masonry-item {
+  display: inline-block;
+  width: 100%;
+  margin-bottom: 1rem;
+  break-inside: avoid;
 }
 
+/* 링크 영역 전체를 클릭 가능 */
 .item-link {
-  display: flex;
-  align-items: center;
+  display: block;
   text-decoration: none;
   color: inherit;
-  flex: 1;
 }
 
+/* 이미지: 컬럼 폭 100% */
 .preview-img {
-  width: 150px;   /* 고정 가로 크기 */
-  height: auto;   /* 세로 비율 유지 */
-  margin-right: 0.75em;
+  width: 100%;
+  height: auto;
+  display: block;
   object-fit: cover;
-  border-radius: 4px;
 }
 
+/* 제목/날짜 스타일 */
 .item-title {
+  display: block;
+  margin: 0.5em 0 0.2em;
   font-size: 1.1rem;
   font-weight: 500;
 }
-
 .item-date {
   font-size: 0.85rem;
   color: #666;
-  margin-left: 0.5em;
 }
 
+/* 글쓰기 버튼 */
 .write-link {
-  margin-top: 1.5rem;
   text-align: right;
+  margin-top: 1.5rem;
 }
-
 .write-link a {
   text-decoration: none;
   color: #409EFF;
